a03eac1f67e55307431915ec82d74e175172cdf0
s4ayub <shababayub@gmail.com>
2016-12-29 03:08:55 -0500

    added notes on bias nodes, added constructor

--- alec.go
+++ alec.go
@@ -8,9 +8,28 @@ import (
 // Use gonum/matrix library for matrix addition, subtraction, mul, dot product, etc.
 
 type Alec struct {
-	hiddenLayers float32
+	binaryThresh float32
 	learningRate float32
-	iterations float32
-	hiddenNeurons float32
+	momentum float32
 }
 
+func NewAlec(bThresh float32, lRate float32, mMentum float32) *Alec { // This is my constructor to instantiate an Alec
+	al := &Alec{
+		binaryThresh: bThresh,
+		learningRate: lRate,
+		momentum: mMentum
+	}
+}
+// need some structs
+// need a constructor. something that instantiates an Alec
+// need a training function, that takes the options of learning rate, and number of hidden layers
+// need forward propagation
+// need back propagation
+// need prediction
+
+	// hiddenLayers float32
+	// learningRate float32
+	// iterations float32
+	// hiddenNeurons float32
+
+// A bias allows you to shift the activation function left or right
