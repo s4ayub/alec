4af4d081d042bb0905bda15e7c5b7c5e5cab2702
s4ayub <shababayub@gmail.com>
2017-01-22 02:23:03 -0500

    add more tests

--- alec_test.go
+++ alec_test.go
@@ -8,24 +8,12 @@ import (
 )
 
 func TestNOR(t *testing.T) {
-	a := alec.Build(0.3, 20000, 5)
-	assert.Equal(t, a.LearningRate, 0.3, "Learning rate should be this value")
+	a := alec.Build(0.2, 50000, 5)
+	assert.Equal(t, a.LearningRate, 0.2, "Learning rate should be this value")
 	assert.Equal(t, a.HiddenNeurons, 5, "Number of hidden neurons should be this value")
-	assert.Equal(t, a.NumOfIterations, 20000, "Number of iterations should be this number")
+	assert.Equal(t, a.NumOfIterations, 50000, "Number of iterations should be this number")
 
 	a.Train([][][]float64{ // NOR truth table
-		{{0, 0}, {1}},
-		{{0, 1}, {0}},
-		{{1, 0}, {0}},
-		{{1, 1}, {0}},
-		{{0, 0}, {1}},
-		{{0, 1}, {0}},
-		{{1, 0}, {0}},
-		{{1, 1}, {0}},
-		{{0, 0}, {1}},
-		{{0, 1}, {0}},
-		{{1, 0}, {0}},
-		{{1, 1}, {0}},
 		{{0, 0}, {1}},
 		{{0, 1}, {0}},
 		{{1, 0}, {0}},
@@ -34,5 +22,23 @@ func TestNOR(t *testing.T) {
 	
 	input := [][]float64{{0, 0}}
 	guess := a.Smart(input)
-	fmt.Println(guess)
+	fmt.Println("NOR Test: ", guess.At(0, 0))
+}
+
+func TestXOR(t *testing.T) {
+	a := alec.Build(0.2, 50000, 5)
+	assert.Equal(t, a.LearningRate, 0.2, "Learning rate should be this value")
+	assert.Equal(t, a.HiddenNeurons, 5, "Number of hidden neurons should be this value")
+	assert.Equal(t, a.NumOfIterations, 50000, "Number of iterations should be this number")
+
+	a.Train([][][]float64{ // NOR truth table
+		{{0, 0}, {0}},
+		{{0, 1}, {1}},
+		{{1, 0}, {1}},
+		{{1, 1}, {0}},
+	})
+	
+	input := [][]float64{{0, 0}}
+	guess := a.Smart(input)
+	fmt.Println("XOR TEST: ", guess.At(0, 0))
 }
